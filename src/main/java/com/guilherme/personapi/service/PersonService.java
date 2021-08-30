ef2f908ac183ce4f2fcae4a30bb5d4e0330b5b86
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-08-30 11:56:40 -0300

    add controller(post), service, dto

--- src/main/java/com/guilherme/personapi/service/PersonService.java
+++ src/main/java/com/guilherme/personapi/service/PersonService.java
@@ -1,2 +1,27 @@
-package com.guilherme.personapi.service;public class PersonService {
+package com.guilherme.personapi.service;
+
+import com.guilherme.personapi.dto.MessageResponseDTO;
+import com.guilherme.personapi.entity.Person;
+import com.guilherme.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
 }
